a7cfcc42de7191dc6e4f7fa6c39aa023c7ed9a95
kwonkidae <kkdosk@naver.com>
2021-09-06 17:14:01 +0900

    add

---
+++ counter/lib/counter/view/counter_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../counter.dart';
+
+class CounterView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(appBar: AppBar(title: const Text("Counter")));
+  }
+}
---
+++ counter/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+
+import 'app.dart';
+import 'counter_observer.dart';
+
+void main() {
+  Bloc.observer = CounterObserver();
+  runApp(const CounterApp());
+}
