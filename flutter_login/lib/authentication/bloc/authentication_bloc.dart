520db7aab1500f35e810cc923ccd7929b263b9e8
kwonkidae <kkdosk@naver.com>
2021-09-10 18:42:18 +0900

    add

---
+++ flutter_login/lib/authentication/bloc/authentication_bloc.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:user_repository/user_repository.dart';
+
+part 'authentication_event.dart';
+part 'authentication_state.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  AuthenticationBloc({
+    required AuthenticationRepository authenticationRepository,
+    required UserRepository userRepository,
+  })  : _authenticationRepository = authenticationRepository,
+        _userRepository = userRepository,
+        super(const AuthenticationState.unknwon()) {}
+
+  final AuthenticationRepository _authenticationRepository;
+  final UserRepository _userRepository;
+}
