a7ed5a97217f1c63635d20f933439af2d5d2d09e
kkdosk <kkdosk@gmail.com>
2021-09-13 07:56:42 +0900

    flutter login

--- flutter_login/lib/authentication/bloc/authentication_bloc.dart
+++ flutter_login/lib/authentication/bloc/authentication_bloc.dart
@@ -14,8 +14,55 @@ class AuthenticationBloc
     required UserRepository userRepository,
   })  : _authenticationRepository = authenticationRepository,
         _userRepository = userRepository,
-        super(const AuthenticationState.unknwon()) {}
+        super(const AuthenticationState.unknwon()) {
+          _authenticationStatusSubscription = _authenticationRepository.status.listen(
+            (status) => add(AuthenticationStatusChanged(status)),
+          );
+        }
 
   final AuthenticationRepository _authenticationRepository;
   final UserRepository _userRepository;
+  late StreamSubscription<AuthenticationStatus> _authenticationStatusSubscription;
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    if (event is AuthenticationStatusChanged) {
+      yield await _mapAuthenticationStatusChangedToState(event);
+    } else if (event is AuthenticationLogoutRequested) {
+      _authenticationRepository.logOut();
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _authenticationStatusSubscription.cancel();
+    _authenticationRepository.dispose();
+    return super.close();
+  }
+
+  Future<AuthenticationState> _mapAuthenticationStatusChangedToState(
+    AuthenticationStatusChanged event,
+  ) async {
+    switch(event.status) {
+      case AuthenticationStatus.unauthenticated:
+        return const AuthenticationState.unauthenticated();
+      case AuthenticationStatus.authenticated:
+        final user = await _tryGetUser();
+        return user != null 
+          ? AuthenticationState.authenticated(user) : const AuthenticationState.unauthenticated();
+      default:
+        return const AuthenticationState.unknwon();
+    }
+  }
+
+  Future<User?> _tryGetUser() async {
+    try {
+      final user = await _userRepository.getUser();
+      return user;
+    } on Exception {
+      return null;
+    }
+  }
 }
